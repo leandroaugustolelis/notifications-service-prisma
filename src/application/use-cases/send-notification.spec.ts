388403befee22c810b7d04fa898f613140231a0e
Leandro Lelis <leandroaugustolelis@gmail.com>
2023-02-21 05:50:39 -0300

    creating notifications tests, using solid principles and fake repository

---
+++ src/application/use-cases/send-notification.spec.ts
@@ -0,0 +1,23 @@
+import { Notification } from "../entities/notification";
+import { SendNotification } from "./send-notification";
+
+const notificationsRepository = {
+  async create(notification: Notification) {
+    console.log(notification)
+  }
+}
+
+describe('Send notification', () => {
+  it('should be able to send a notification', async () => {
+    const sendNotification = new SendNotification(notificationsRepository)
+
+    const { notification } = await sendNotification.execute({
+      content: 'This is a notification',
+      category: 'social',
+      recipientId: 'example-recipient-id'
+    })
+
+    expect(notification).toBeTruthy()
+
+  })
+})
